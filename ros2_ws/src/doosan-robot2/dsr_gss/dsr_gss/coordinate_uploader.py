bc47a032ffb79989cba6485041d9921fc963113d
HYUNKEUN PARK <hermes7308@gmail.com>
2025-11-17 10:22:19 +0900

    Refactor CoordinateUploader: streamline joint state handling and improve logging; remove unused joint position service

--- ros2_ws/src/doosan-robot2/dsr_gss/dsr_gss/coordinate_uploader.py
+++ ros2_ws/src/doosan-robot2/dsr_gss/dsr_gss/coordinate_uploader.py
@@ -2,26 +2,41 @@ import rclpy
 from rclpy.node import Node
 from dsr_msgs2.srv import GetCurrentPosx
 from dsr_msgs2.srv import GetCurrentPosj
+from sensor_msgs.msg import JointState
 
+import math
 import time
-from dsr_gss.firebase_utils import get_firebase_db_reference
+from dsr_gss.firebase_utils import get_firebase_db_reference, info
 
 
 class CoordinateUploader(Node):
     def __init__(self):
         super().__init__("coordinate_uploader")
         self.db_ref = get_firebase_db_reference()
-        self.timer = self.create_timer(3.0, self.timer_callback)
+        self.create_subscription(
+            JointState, "/dsr01/joint_states", self.joint_callback, 10  # 절대 경로
+        )
+        self.timer = self.create_timer(1.0, self.timer_callback)
 
         self.task_cli = self.create_client(
             GetCurrentPosx, "dsr01/aux_control/get_current_posx"
         )
-        self.joint_cli = self.create_client(
-            GetCurrentPosj, "/dsr01/aux_control/get_current_posj"
-        )
 
         self.task_req = GetCurrentPosx.Request()
-        self.joint_req = GetCurrentPosj.Request()
+
+    def joint_callback(self, msg):
+        """joint_states 콜백: 데이터 저장만 수행"""
+        # msg.name: ['joint_1', 'joint_2', 'joint_4', 'joint_5', 'joint_3', 'joint_6']
+        # 0, 1, 4, 2, 3, 5
+        global joint_cooridnates
+
+        joint_index_list = [0, 1, 4, 2, 3, 5]
+        joint_cooridnates = [
+            math.degrees(msg.position[idx]) for idx in joint_index_list
+        ]
+        self.db_ref.child("joint_coordinate").set(
+            {"data": joint_cooridnates, "timestamp": int(time.time() * 1000)}
+        )
 
     def timer_callback(self):
         print("좌표계 업로더 동작 중...")
@@ -29,14 +44,8 @@ class CoordinateUploader(Node):
         while not self.task_cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("서비스 대기 중: dsr01/aux_control/get_current_posx")
         task_future = self.task_cli.call_async(self.task_req)
-        print("서비스 호출 중: dsr01/aux_control/get_current_posx")
+        self.get_logger().info("서비스 호출 중: dsr01/aux_control/get_current_posx")
         task_future.add_done_callback(self.task_response_callback)
-
-        while not self.joint_cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info("서비스 대기 중: dsr01/aux_control/get_current_posj")
-        joint_future = self.joint_cli.call_async(self.joint_req)
-        print("서비스 호출 중: dsr01/aux_control/get_current_posj")
-        joint_future.add_done_callback(self.joint_response_callback)
 
     def task_response_callback(self, future):
         try:
@@ -46,19 +55,7 @@ class CoordinateUploader(Node):
                 self.db_ref.child("task_coordinate").set(
                     {"data": data, "timestamp": int(time.time() * 1000)}
                 )
-                print(f"Task 좌표계 데이터 수신(콜백): {data}")
-        except Exception as e:
-            self.get_logger().error(f"서비스 호출 중 오류 발생(콜백): {e}")
-
-    def joint_response_callback(self, future):
-        try:
-            response = future.result()
-            if response.success:
-                data = [p for p in response.pos]
-                self.db_ref.child("joint_coordinate").set(
-                    {"data": data, "timestamp": int(time.time() * 1000)}
-                )
-                print(f"Joint 좌표계 데이터 수신(콜백): {data}")
+                self.get_logger().info(f"Task 좌표계 데이터 수신(콜백): {data}")
         except Exception as e:
             self.get_logger().error(f"서비스 호출 중 오류 발생(콜백): {e}")
 
@@ -69,6 +66,7 @@ def main(args=None):
     node = CoordinateUploader()
 
     try:
+        info("CoordinateUploader 시작합니다.")
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
